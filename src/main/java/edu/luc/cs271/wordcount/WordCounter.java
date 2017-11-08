b6cbd8c7cda66257d6da20da774c45b2532401d5
Kurt Daill <kdaill@luc.edu>
2017-11-08 00:30:54 +0000

    WordCounter Works

--- src/main/java/edu/luc/cs271/wordcount/WordCounter.java
+++ src/main/java/edu/luc/cs271/wordcount/WordCounter.java
@@ -12,15 +12,17 @@ public class WordCounter {
 
   /** Creates a word counter instance based on the given map. */
   public WordCounter(final Map<String, Integer> theMap) {
-
-    // TODO
-    this.theMap = null;
+    this.theMap = theMap;
 
   }
 
   /** Counts the frequencies of all words in the given iterator. */
   public void countWords(final Iterator<String> words) {
-
+    String tempString;
+    while(words.hasNext()){
+      tempString = words.next();
+      theMap.put(tempString, theMap.getOrDefault(tempString, 0) + 1);
+    }
     // TODO for each word in the iterator, update the corresponding frequency in the map
     // HINT to do this without a conditional, use the getOrDefault method
 
@@ -28,9 +30,7 @@ public class WordCounter {
 
   /** Retrieve the frequency of a particular word. */
   public int getCount(final String word) {
-
-    // TODO
-    return -1;
+    return theMap.getOrDefault(word, 0);
 
   }
 
--- src/test/java/edu/luc/cs271/wordcount/TestWordCounter.java
+++ src/test/java/edu/luc/cs271/wordcount/TestWordCounter.java
@@ -5,38 +5,51 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.ArrayList;
 
 public class TestWordCounter {
 
   // TODO complete this test class
 
   // TODO declare a reference to the SUT (system under test), i.e., WordCounter
+  private WordCounter fixture;
 
   @Before
   public void setUp() {
-    // TODO create the SUT instance
+    fixture = new WordCounter(new HashMap<String, Integer>());
   }
 
   @After
   public void tearDown() {
-    // TODO set the SUT instance to null
+    fixture = null;
   }
 
   @Test
   public void testGetCountEmpty() {
-
     // TODO verify that the SUT initially returns an empty map
-    fail();
-
+    assertEquals(new HashMap<String, Integer>(), fixture.getCounts());
   }
 
   @Test
   public void testGetCountNonEmpty() {
-
+    ArrayList<String> list = new ArrayList<String>();
+    list.add("Three");
+    list.add("Three");
+    list.add("Three");
+    list.add("One");
+    Iterator<String> listI = list.iterator();
+    
+    fixture.countWords(listI);
+    assertEquals(3, fixture.getCount("Three"));
+    assertEquals(1, fixture.getCount("One"));
+    assertEquals(0, fixture.getCount("Zero"));
+    assertEquals(0, fixture.getCount("Number"));
     // TODO run the SUT on a specific String iterator with some repeated words,
     // then use assertions to verify the correct counts
     // do this for at least two words in the iterator and two not in the iterator
-    fail();
-
   }
 }
